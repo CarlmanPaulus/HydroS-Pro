be3f301d520612611d7a51b3743b79c648e01fb1
CarlmanPaulus <ml8340654@gmail.com>
2026-04-23 11:26:27 +0800

    Remove all files except README.md and version.txt

--- RAW_combiner/__init__.py
+++
@@ -1,5 +0,0 @@
-"""RAW Files Combiner package."""
-
-from .main_window import APP_ID, CURRENT_VERSION, WINDOW_TITLE, CombinerWidget
-
-__all__ = ["APP_ID", "CURRENT_VERSION", "WINDOW_TITLE", "CombinerWidget"]
