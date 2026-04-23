77f655f25f74094b0c1bc3b3215dd903432f683f
CarlmanPaulus <ml8340654@gmail.com>
2026-04-23 11:31:10 +0800

    Remove all files except README.md and version.txt

--- manual_extractor/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Manual Extractor package."""
-
-from .main_window import APP_ID, CURRENT_VERSION, WINDOW_TITLE, ExtractorWidget
-
-__all__ = ["APP_ID", "CURRENT_VERSION", "WINDOW_TITLE", "ExtractorWidget"]
